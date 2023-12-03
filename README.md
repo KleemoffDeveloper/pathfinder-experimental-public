70d5bc2e7e68c32066d5e8be1425a3602c8a8151
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-12-03 16:51:16 -0500

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align=center>
+<img src="./src/assets/PathFinder-small.png">
+<h2>Pathfinder Ver. 1</h2>
+</div>
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <div className="app grid place-items-center min-h-screen">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
